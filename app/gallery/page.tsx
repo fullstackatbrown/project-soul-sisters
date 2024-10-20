ae3e739c909a634f8f056a94f3244fcc7557b9c7
Aanya Agrawal <purplishwings@gmail.com>
2024-10-20 17:19:38 -0400

    added basic gallery with placeholders

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,5 +1,14 @@
+"use client";
+import { useState } from 'react';
+import GalleryPhoto from "../../components/GalleryPhoto/GalleryPhoto";
+import styles from "./gallery.module.css";
+
 export default function Gallery() {
+  const [photos, setPhotos] = useState(["https://media.istockphoto.com/id/175397603/photo/frog.jpg?s=612x612&w=0&k=20&c=EMXlwg5SicJllr7gnSFUUjzwCGa1ciLjYD1bk8NvO2E=", "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png", "https://cdn.britannica.com/73/100273-050-221A0593/arrow-poison-frogs-sound-production-colour-warning-signal.jpg"]);
     return (
-      <div></div>
+
+      <div className="flex flex-wrap">{photos.map((x, i) =>
+        <GalleryPhoto url={x} /> 
+        )}</div>
     );
   }
