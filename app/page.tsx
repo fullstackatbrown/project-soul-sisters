d6b3c0e784b837de0c3d1185c1a9f41204cf97ec
monoguitari <jesus_rodriguez@brown.edu>
2024-11-03 18:22:32 -0500

    feat: cosmicClient documentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,20 @@
 import cosmic from "../lib/cosmicClient";
 
 export default async function Home() {
-  let data = await cosmic.objects
-    .findOne({
-      type: "home-video",
-      slug: "main-page-background-video",
-    })
+  const query = {
+    type: "main-page-video",
+    slug: "background-video",
+  };
+  let response = await cosmic.objects
+    .findOne(query)
     .props("slug,title,metadata")
     .depth(1);
-  console.log("Fetched data:", JSON.stringify(data, null, 2));
-  console.log("Metadata:", data);
+  // console.log("Fetched data:", JSON.stringify(data, null, 2));
+  // console.log("Metadata:", data);
   return (
     <div>
       <video
-        src={data.object.metadata.video.url}
+        src={response.object.metadata.video.url}
         autoPlay
         muted
         loop
--- lib/cosmicClient.ts
+++ lib/cosmicClient.ts
@@ -2,8 +2,10 @@ import { createBucketClient } from "@cosmicjs/sdk";
 
 //https://www.cosmicjs.com/docs/frameworks#next-js
 /**
+ *  We also have reading on the README.md on how to use this client
  * The above is a link to the nextjs cosmicjs tutorial and documentation.
  * All we have to do is import cosmic and query from that rather than always creating a new client
+ * NOTE: to reference an object you queried, its always <data you got>.object.<field>. Can console.log(data) to see the structure
  */
 const cosmic = createBucketClient({
   bucketSlug: process.env.BUCKET_SLUG || "",
