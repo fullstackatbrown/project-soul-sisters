b9de648a5333b98102733070bb521e10cd29faa2
monoguitari <jesus_rodriguez@brown.edu>
2024-11-03 14:10:03 -0500

    feat: simple cosmic video return

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,29 @@
-export default function Home() {
+import { createBucketClient } from "@cosmicjs/sdk";
+
+const cosmic = createBucketClient({
+  bucketSlug: process.env.BUCKET_SLUG || "",
+  readKey: process.env.BUCKET_READ_KEY || "",
+});
+
+export default async function Home() {
+  let data = await cosmic.objects
+    .findOne({
+      type: "home-video",
+      slug: "main-page-background-video",
+    })
+    .props("slug,title,metadata")
+    .depth(1);
+  console.log("Fetched data:", JSON.stringify(data, null, 2));
+  console.log("Metadata:", data);
   return (
-    <div></div>
+    <div>
+      <video
+        src={data.object.metadata.video.url}
+        autoPlay
+        muted
+        loop
+        playsInline
+      />
+    </div>
   );
 }
