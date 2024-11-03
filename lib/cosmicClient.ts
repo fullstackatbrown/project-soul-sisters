270651230558511298f7fd1607cd51571b498ed3
monoguitari <jesus_rodriguez@brown.edu>
2024-11-03 14:19:56 -0500

    doc: documentation

--- lib/cosmicClient.ts
+++ lib/cosmicClient.ts
@@ -1,5 +1,10 @@
 import { createBucketClient } from "@cosmicjs/sdk";
 
+//https://www.cosmicjs.com/docs/frameworks#next-js
+/**
+ * The above is a link to the nextjs cosmicjs tutorial and documentation.
+ * All we have to do is import cosmic and query from that rather than always creating a new client
+ */
 const cosmic = createBucketClient({
   bucketSlug: process.env.BUCKET_SLUG || "",
   readKey: process.env.BUCKET_READ_KEY || "",
