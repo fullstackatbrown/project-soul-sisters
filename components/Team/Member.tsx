73380b9275421150091ea31c1a5f4aee8363c845
monoguitari <jesus_rodriguez@brown.edu>
2024-11-17 15:02:15 -0500

    feat: star names

--- components/Team/Member.tsx
+++ components/Team/Member.tsx
@@ -1,19 +1,35 @@
+import React from "react";
 import styles from "./Team.module.css";
+import { Starname } from "./Team";
 
 interface MemberProps {
   role: string;
   name: string;
   imageUrl: string;
+  star_names?: Starname[];
 }
 
-export default function Member({ role, name, imageUrl }: MemberProps) {
+export default function Member(props: MemberProps) {
   return (
     <div className={styles.teamMember}>
       <div className={styles.memberLeft}>
-        <p>{role}</p>
-        <p>{name}</p>
+        {props.star_names ? (
+          <>
+            <p>{props.role}</p>
+            {props.star_names.map((star, i) => (
+              <p key={i}>{star.name}</p>
+            ))}
+          </>
+        ) : (
+          <>
+            <>
+              <p>{props.role}</p>
+              <p>{props.name}</p>
+            </>
+          </>
+        )}
       </div>
-      <img src={imageUrl} className={styles.memberPhoto} />
+      <img src={props.imageUrl} className={styles.memberPhoto} />
     </div>
   );
 }
--- components/Team/Team.tsx
+++ components/Team/Team.tsx
@@ -3,6 +3,10 @@ import cosmic from "../../lib/cosmicClient";
 import styles from "./Team.module.css";
 import Member from "./Member";
 
+export interface Starname {
+  name: string;
+}
+
 export default async function Team() {
   const data = await cosmic.objects
     .find({
@@ -11,6 +15,15 @@ export default async function Team() {
     .props("slug,title,metadata")
     .depth(1);
 
+  const starring = await cosmic.objects
+    .findOne({
+      type: "starring",
+      slug: "starring-members",
+    })
+    .props("slug,title,metadata,type")
+    .depth(1);
+
+  console.log(starring);
   return (
     <div className={styles.teamContainer}>
       <h1 className={styles.teamTitle}>Production Team</h1>
@@ -23,6 +36,12 @@ export default async function Team() {
             key={i}
           />
         ))}
+        <Member
+          role="Starring"
+          name="Starring Members"
+          imageUrl="/images/starring.jpg"
+          star_names={starring.object.metadata.star_names as Starname[]}
+        />
       </div>
     </div>
   );
