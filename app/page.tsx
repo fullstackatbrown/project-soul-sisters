d7556ab09d3e9bc4fb123ddd50653ff17dc28807
Programmer707 <replydeveshkumar@gmail.com>
2024-11-24 04:49:40 -0500

    all done

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,26 @@
-import cosmic from "../lib/cosmicClient";
+// import cosmic from "../lib/cosmicClient";
 
 export default async function Home() {
-  const query = {
-    type: "main-page-video",
-    slug: "background-video",
-  };
-  let response = await cosmic.objects
-    .findOne(query)
-    .props("slug,title,metadata")
-    .depth(1);
+  // const query = {
+  //   type: "main-page-video",
+  //   slug: "background-video",
+  // };
+  // let response = await cosmic.objects
+  //   .findOne(query)
+  //   .props("slug,title,metadata")
+  //   .depth(1);
   // console.log("Fetched data:", JSON.stringify(data, null, 2));
   // console.log("Metadata:", data);
   return (
     <div>
-      <video
+      {/* <video
         src={response.object.metadata.video.url}
         autoPlay
         muted
         loop
         playsInline
-      />
+      /> */}
+      <p>hi</p>
     </div>
   );
 }
