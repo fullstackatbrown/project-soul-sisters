b27c2a1961190b7030122617cbdeeee05de1a278
xuvi7 <07xuvi@gmail.com>
2024-12-04 16:48:01 -0500

    perf: decrease image load time

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -4,13 +4,13 @@ import cosmic from '@/lib/cosmicClient'
 // padding, center, prob 3 columns
 
 export default async function Gallery() {
-let vals = await cosmic.media.find({"folder":"gallery"})
-.limit(50)
-.props("url,name")
+  let vals = await cosmic.media.find({ "folder": "gallery" })
+    .limit(50)
+    .props("url,name,alt_text,width,height")
 
-    return (
-      <div>
-        <GalleryDisplay urls={vals} />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <GalleryDisplay urls={vals} />
+    </div>
+  );
+}
