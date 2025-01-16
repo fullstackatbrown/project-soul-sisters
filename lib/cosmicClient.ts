20dc3460122b9fd8f6d424b73defe21d4a748dc2
xuvi7 <07xuvi@gmail.com>
2025-01-16 11:10:31 -0500

    remove api keys outside of .env

--- lib/cosmicClient.ts
+++ lib/cosmicClient.ts
@@ -8,8 +8,8 @@ import { createBucketClient } from "@cosmicjs/sdk";
  * NOTE: to reference an object you queried, its always <data you got>.object.<field>. Can console.log(data) to see the structure
  */
 const cosmic = createBucketClient({
-  bucketSlug: process.env.BUCKET_SLUG || "soul-sisters-production-c52519c0-9a32-11ef-9152-c3825c893765",
-  readKey: process.env.BUCKET_READ_KEY || "toT4sKELwi3M0xzs4s9DXwNnWV1X64ACd8662cbH1cjMEGV79W",
+  bucketSlug: process.env.BUCKET_SLUG || "",
+  readKey: process.env.BUCKET_READ_KEY || "",
 });
 
 export default cosmic;
